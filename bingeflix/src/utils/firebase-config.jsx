034dbf6d699c7324d14e529b4f1d8361558c4952
Reena kumari <reenakvs1@gmail.com>
2023-11-21 21:54:30 +0530

    changes made

--- bingeflix/src/utils/firebase-config.jsx
+++ bingeflix/src/utils/firebase-config.jsx
@@ -8,9 +8,11 @@ const firebaseConfig = {
   
   projectId: "bingeflix-7fafe",
   
+
   storageBucket: "bingeflix-7fafe.appspot.com",
   messagingSenderId: "1042894222478",
   appId: "1:1042894222478:web:541b4929414708bc5fc5df",
+  
   measurementId: "G-NXFNQF9ZTQ"
 };
 
