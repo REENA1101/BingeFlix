c7313e4dd7ab2c7ffff4b5ecc9e98f68e7cbab72
Reena kumari <reenakvs1@gmail.com>
2023-12-27 17:31:02 +0530

    Hero Page buttons created and movie rendered

--- bingeflix/src/pages/BingeFlix.jsx
+++ bingeflix/src/pages/BingeFlix.jsx
@@ -5,6 +5,7 @@ import {AiOutlineInfoCircle} from 'react-icons/ai';
 import { FaPlay } from 'react-icons/fa';
 import backgroundImage from "../assets/home.jpg";
 import MovieLogo from "../assets/homeTitle.webp";
+import styled from 'styled-components';
 
 
 export default function BingeFlix() {
@@ -16,7 +17,7 @@ export default function BingeFlix() {
    }
 
   return (
-    <div>
+    <Container>
       <Navbar isScrolled={isScrolled}/>
 
       <div className="hero">
@@ -31,7 +32,7 @@ export default function BingeFlix() {
            <img src={MovieLogo} alt="Movie Logo"/>
         </div>
 
-        <div className="button flex">
+        <div className="buttons flex">
           <button className="flex j-center a-center">
              <FaPlay/> Play
           </button>
@@ -41,10 +42,59 @@ export default function BingeFlix() {
           </button>
         </div>
       </div>
-
-
       </div>
-  
-    </div>
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+background-color:black;
+.hero{
+  position:relative;
+  .background-image{
+      filter: brightness(60%);
+  }
+  img{
+    height: 110vh;
+    width: 100vw;
+  }
+  .container{
+    position: absolute;
+    bottom:5rem;
+  }
+  .logo{
+    img{
+      width: 100%;
+      height: 100%;
+      margin-left:5rem;
+    }
+  }
+  .buttons{
+     margin: 5rem;
+     gap:2rem;
+     button{
+      font-size: 1rem;
+      gap:1rem;
+      border-radius:0.2rem;
+      padding: 0.3rem;
+      padding-left:2rem;
+      padding-right:2.4rem;
+      border:none;
+      cursor: pointer;
+      transition:0.2s ease-in-out;
+      &:hover{
+        opacity: 0.8;
+      }
+      &:nth-of-type(2){
+           background-color: rgba(109, 109, 110, 0.7);
+           color:white;
+           svg {
+            font-size:1.8rem;
+           }
+        }
+      }
+    }
+  }
+}
+`;
