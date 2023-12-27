dc0741bd4676490ab7ceca76bef4869dec91ffb5
Reena kumari <reenakvs1@gmail.com>
2023-12-27 17:07:59 +0530

    Hero Page started and created and navbar scroll completed and background image and movie logo rendered successfully

--- bingeflix/src/pages/BingeFlix.jsx
+++ bingeflix/src/pages/BingeFlix.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { useState } from 'react';
 import Navbar from '../components/Navbar';
+import {AiOutlineInfoCircle} from 'react-icons/ai';
+import { FaPlay } from 'react-icons/fa';
+import backgroundImage from "../assets/home.jpg";
+import MovieLogo from "../assets/homeTitle.webp";
+
 
 export default function BingeFlix() {
    const [isScrolled, setIsScrolled] = useState(false);
@@ -13,6 +18,33 @@ export default function BingeFlix() {
   return (
     <div>
       <Navbar isScrolled={isScrolled}/>
+
+      <div className="hero">
+      <img 
+        src ={backgroundImage} 
+        alt ="background" 
+        className="background-image"
+      />
+
+      <div className="container">
+        <div className="logo">
+           <img src={MovieLogo} alt="Movie Logo"/>
+        </div>
+
+        <div className="button flex">
+          <button className="flex j-center a-center">
+             <FaPlay/> Play
+          </button>
+
+          <button className="flex j-center a-center">
+             <AiOutlineInfoCircle/> More Info
+          </button>
+        </div>
+      </div>
+
+
+      </div>
+  
     </div>
   )
 }
