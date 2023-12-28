e0dd3aac3dd5c3131abc458ba78ecaf751e74929
Reena kumari <reenakvs1@gmail.com>
2023-12-28 08:30:32 +0530

    Fetching Movies from TMDB API

--- bingeflix/src/store/index.js
+++ bingeflix/src/store/index.js
@@ -14,7 +14,37 @@ export const getGenres = createAsyncThunk("netflix/genres", async()=>{
     } = await axios.get(`${TMDB_BASE_URL}/genre/movie/list?api_key=${API_KEY}`
     );
     return genres;
-}); 
+});
+
+const createArrayFromRawData = (array, movieArray, genres)=>{
+    array.forEach(()=>{
+        const movieGenres = [];
+        movie.genre_id
+    })
+}
+
+const getRawData = (api,genres, paging )=>{
+    const movieArray = [];
+    for(let i=1; movieArray.length<60 && i<10; i++){
+       const {data:results} = await axios.get(`${api}${paging?`&page=${i}`:""}`
+          createArrayFromRawData(results, moviesArray, genres);
+       );
+       return movieArray;
+    }
+};
+
+export const fetchMovies = createAsyncThunk("netflix/trending", async({type}, thunkApi)=>{
+    const {
+        bingefleix:{genres},
+    } =thunkApi.getState();
+     return getRawData(
+        `${TMBD_BASE_URL}/trending/${type}/week?api_key=${API_KEY}`,
+        genres,
+        true)
+}
+);
+
+// return getRawData(`${TMBD_BASE_URL}/discover/${type}?api_key=${API_KEY}&with_genres=&{genre}`)
 
 const BingeflixSlice = createSlice({
     name: "NetFlix",
