ada28da37c55da87c61de5f277985c6a7d84c995
Reena kumari <reenakvs1@gmail.com>
2023-12-31 00:30:26 +0530

    the card hover video nicely styled

--- bingeflix/src/components/Card.jsx
+++ bingeflix/src/components/Card.jsx
@@ -36,8 +36,8 @@ export default function Card({movieData, isLiked=false}) {
                    />
                 </div>
 
-               <div className='.info-container flex column'>
-                  <h3 className='name' onClick={()=> navigate('/player')}>
+               <div className='.info-container flex column' style={{padding:"1rem"}}>
+                  <h3 className='name' onClick={()=> navigate('/player')} >
                     {movieData.name}
                   </h3>
 
@@ -62,7 +62,7 @@ export default function Card({movieData, isLiked=false}) {
                        </div>
                 </div>
 
-                <div className="genres flex">
+                <div className="genres flex" >
                     <ul className="flex">{movieData.genres.map((genre)=>(
                            <li key={genre}>{genre}</li>
                     ))}
@@ -78,48 +78,79 @@ export default function Card({movieData, isLiked=false}) {
 const Container = styled.div`
 max-width: 230px;
 width: 230px;
-height:100%;
-cursor:pointer;
-position:relative;
-img{
-    border-radius:0.2rem;
-    width:100%;
-    height:100%;
-    z-index:10;
+height: 100%;
+cursor: pointer;
+position: relative;
+img {
+  border-radius: 0.2rem;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
 }
-.hover{
-    z-index: 90;
-    height: max-content;
-    width: 20rem;
-    position:absolute;
-    top: -18vh;
-    left:0;
-    border-radius:0.3rem;
-    box-shadow: rgba(0,0,0,0.75) 0px 3px 10px;
-    background-color: #181818;
-    transition: 0.3s ease-in-out;
-    .image-video-container{
-        position:relative;
-        height:140px;
-         img{
-            width:100%;
-            height: 140px;
-            object-fit:cover;
-            border-radius: 0.3rem;
-            top: 0;
-            z-index: 4;
-            position:absolute;
-         }
-         video{
-            
-            width:100%;
-            height:140px;
-            object-fit: cover;
-            border-radius:0.3rem;
-            top: 0;
-            z-index:5;
-            position:absolute;
-         }
+.hover {
+  z-index: 99;
+  height: max-content;
+  width: 20rem;
+  position: absolute;
+  top: -18vh;
+  left: 0;
+  border-radius: 0.3rem;
+  box-shadow: rgba(0, 0, 0, 0.75) 0px 3px 10px;
+  background-color: #181818;
+  transition: 0.3s ease-in-out;
+  .image-video-container {
+    position: relative;
+    height: 140px;
+    img {
+      width: 100%;
+      height: 140px;
+      object-fit: cover;
+      border-radius: 0.3rem;
+      top: 0;
+      z-index: 4;
+      position: absolute;
     }
+    video {
+      width: 100%;
+      height: 140px;
+      object-fit: cover;
+      border-radius: 0.3rem;
+      top: 0;
+      z-index: 5;
+      position: absolute;
+    }
+  }
+  .info-container {
+    padding: 1rem;
+    gap: 0.5rem;
+  }
+  .icons {
+    .controls {
+      display: flex;
+      gap: 1rem;
+    }
+    svg {
+      font-size: 1.4rem;
+    //   margin-left:1rem;
+      margin-top:0.5rem;
+      gap:2rem;
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+      &:hover {
+        color: #b8b8b8;
+      }
+    }
+  }
+  .genres {
+    ul {
+      gap: 1rem;
+      li {
+        padding-right: 0.7rem;
+        &:first-of-type {
+          list-style-type: none;
+        }
+      }
+    }
+  }
 }
 `;
