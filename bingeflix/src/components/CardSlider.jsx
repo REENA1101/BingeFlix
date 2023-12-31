4fe8fe316445703bee35f5fd0a1d62c181ac8928
Reena kumari <reenakvs1@gmail.com>
2023-12-31 15:08:41 +0530

    code optimization using react memohook

--- bingeflix/src/components/CardSlider.jsx
+++ bingeflix/src/components/CardSlider.jsx
@@ -3,7 +3,7 @@ import Card from './Card'
 import styled from 'styled-components';
 import {  AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
 
-export default function CardSlider({data,title}) {
+export default React.memo(function CardSlider({data,title}) {
    const [showControls, setShowControls] = useState(false);
    const [sliderPosition, setSliderPosition] = useState(0);
    const listRef = useRef();
@@ -53,6 +53,7 @@ export default function CardSlider({data,title}) {
     </Container>
   );
 }
+);
 
 const Container = styled.div`
   gap:1rem;
