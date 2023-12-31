2a869c8e8f9cfeadfc9cb74c1a99736d8d9fb0f3
Reena kumari <reenakvs1@gmail.com>
2023-12-31 15:04:52 +0530

    card slider successfullycompleted

--- bingeflix/src/components/CardSlider.jsx
+++ bingeflix/src/components/CardSlider.jsx
@@ -8,11 +8,18 @@ export default function CardSlider({data,title}) {
    const [sliderPosition, setSliderPosition] = useState(0);
    const listRef = useRef();
 
-   const handleDirection=(direction)=>{
-
-   }
-
-
+   const handleDirection = (direction) => {
+      console.log(useRef, direction)
+    let distance = listRef.current.getBoundingClientRect().x - 70;
+    if (direction === "left" && sliderPosition > 0) {
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      setSliderPosition(sliderPosition - 1);
+    }
+    if (direction === "right" && sliderPosition < 4) {
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      setSliderPosition(sliderPosition + 1);
+    }
+  };
 
    return (
     <Container
@@ -50,9 +57,9 @@ export default function CardSlider({data,title}) {
 const Container = styled.div`
   gap:1rem;
   position:relative;
-  padding: 2rem 0;
+  padding: 1rem 0;
   h1{
-    margin-left: 50px;
+    margin-left: 40px;
   }
   .wrapper{
     .slider{
@@ -60,7 +67,29 @@ const Container = styled.div`
       gap:1rem;
       transform:translateX(0px);
       transition: 0.3s ease-in-out;
-      margin-left: 50px;
+      margin-left: 35px;
+    }
+    .slider-action{
+      position: absolute;
+      z-index:99;
+      height:100%;
+      top:0;
+      bottom: 40px;
+      transition: 0.3s ease-in-out;
+
+      svg{
+        font-size: 1.8rem;
+        margin-top:3rem;
+            }
+    }
+    .none{
+       display:none;
+    }
+    .left{
+      left:0;
+    }
+    .right{
+      right: 0;
     }
   }
 `;
--- bingeflix/src/components/Slider.jsx
+++ bingeflix/src/components/Slider.jsx
@@ -7,6 +7,8 @@ export default function Slider({movies}) {
         return movies.slice(from,to)
     }
 
+    console.log(movies)
+
   return (
     <div>
         <CardSlider title = "Trending Now" data={getMoviesFromRange(0,10)}/>
