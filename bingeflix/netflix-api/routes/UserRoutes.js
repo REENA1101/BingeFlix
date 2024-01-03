935d6c6c7b805c272252c83bf4722ce79bf8494f
Reena kumari <reenakvs1@gmail.com>
2024-01-03 17:20:10 +0530

    node.js API add movie to Mongo DB

---
+++ bingeflix/netflix-api/routes/UserRoutes.js
@@ -0,0 +1,7 @@
+const { addToLikedMovies } = require("../controllers/userController");
+
+const router = require("express").Router();
+
+router.post("/add", addToLikedMovies);
+
+module.exports =router;
