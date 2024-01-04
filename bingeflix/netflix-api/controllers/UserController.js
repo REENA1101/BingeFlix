a06df3ffd2d6016454b799d7784317be57b01d17
Reena kumari <reenakvs1@gmail.com>
2024-01-04 08:40:14 +0530

    mongo db user db adding error

--- bingeflix/netflix-api/controllers/UserController.js
+++ bingeflix/netflix-api/controllers/UserController.js
@@ -1,15 +1,13 @@
 const User = require("../models/UserModel");
 
 
-
-
 module.exports.addToLikedMovies = async (req, res) => {
     try {
       const { email, data } = req.body;
-      const user = await await User.findOne({ email });
+      const user = await User.findOne({ email });
       if (user) {
         const { likedMovies } = user;
-        const movieAlreadyLiked = likedMovies.find(({ id }) => id === data.id);
+        const movieAlreadyLiked = likedMovies.find(({ id }) => (id = data.id));
         if (!movieAlreadyLiked) {
           await User.findByIdAndUpdate(
             user._id,
@@ -22,6 +20,6 @@ module.exports.addToLikedMovies = async (req, res) => {
       } else await User.create({ email, likedMovies: [data] });
       return res.json({ msg: "Movie successfully added to liked list." });
     } catch (error) {
-      return res.json({ msg: "Error adding movie to the liked list" });
+      return res.json({ msg: "Error adding movie" });
     }
   };
